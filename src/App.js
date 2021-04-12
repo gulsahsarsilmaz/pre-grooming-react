a0257e8129f4988c6a12ec97ceafdafab20dc5a1
gulsahsarsilmaz <sarsilmazgulsah@gmail.com>
2021-04-12 10:58:44 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import { Typography, Button } from "@material-ui/core";
 export default function App() {
   const [data, setData] = useState({
     users: [
-      { id: 1, value: "Eric", isChecked: true, isCurrent: false },
-      { id: 2, value: "Gulsah", isChecked: true, isCurrent: false },
+      { id: 1, value: "Eric", isChecked: false, isCurrent: false },
+      { id: 2, value: "Gulsah", isChecked: true, isCurrent: true },
       { id: 3, value: "David", isChecked: true, isCurrent: false },
-      { id: 4, value: "Shreya", isChecked: true, isCurrent: false },
-      { id: 5, value: "Jay", isChecked: true, isCurrent: true },
-      { id: 6, value: "Louis", isChecked: true, isCurrent: true },
-      { id: 7, value: "Thomas", isChecked: true, isCurrent: true },
-      { id: 8, value: "Baran", isChecked: true, isCurrent: true }
+      { id: 4, value: "Shreya", isChecked: true, isCurrent: true },
+      { id: 5, value: "Jay", isChecked: false, isCurrent: false },
+      { id: 6, value: "Louis", isChecked: true, isCurrent: false },
+      { id: 7, value: "Thomas", isChecked: true, isCurrent: false },
+      { id: 8, value: "Baran", isChecked: true, isCurrent: false }
     ]
   });
 
