057af803cdd81b90901f52342e604995afd2d77e
gulsahsarsilmaz <sarsilmazgulsah@gmail.com>
2021-05-08 18:13:02 -0400

    1 file modified

--- src/data/core-blue.js
+++ src/data/core-blue.js
@@ -1,8 +1,8 @@
 export const coreBlue = {
   users: [
-    { id: 1, value: "Shreya", isChecked: false, isCurrent: false },
-    { id: 4, value: "Thomas", isChecked: false, isCurrent: false },
-    { id: 7, value: "Baran", isChecked: true, isCurrent: true },
-    { id: 8, value: "Marlee", isChecked: false, isCurrent: false }
+    { id: 1, value: 'Shreya', isChecked: false, isCurrent: false },
+    { id: 4, value: 'Thomas', isChecked: true, isCurrent: true },
+    { id: 7, value: 'Baran', isChecked: true, isCurrent: false },
+    { id: 8, value: 'Marlee', isChecked: false, isCurrent: false }
   ]
 };
